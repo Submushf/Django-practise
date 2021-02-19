c217920dab144396aaa29202689a4177364935c1
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-02-19 18:11:36 +0600

    update

--- Mylist/views.py
+++ Mylist/views.py
@@ -1,4 +1,8 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
+BASE_CRAIGSLIST_URL = 'https://dhaka.craigslist.org/search/?query={}'
 
 # Create your views here.
 def home(request):
