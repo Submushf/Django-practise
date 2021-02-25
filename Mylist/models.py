e880eaf980923b05f1ab74a6dd0ca14ddd109777
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-02-25 13:57:49 +0600

    new update

--- Mylist/models.py
+++ Mylist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Search(models.Model):
     search = models.CharField(max_length=500)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now=False)
 
     def __str__(self):
         return '{}'.format(self.search)
